0ff6af839e7aa31b896e22b1389b66fcf6eb36c3
Joaquín Fernández <jf@joaquinfernandez.net>
2019-04-17 20:50:32 +0200

    Migración a @jf

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
-const regexcape = require('regexcape');
+const regexcape = require('@jf/regexcape');
 
 /**
  * Función para convertir a formato camelCase cualquier texto separado por
  * el separador especificado.
  *
- * @param {String} text      Texto a convertir.
- * @param {String} separator Separador usado.
+ * @param {string}  text       Texto a convertir.
+ * @param {string}  separator  Caracteres usados como separador.
+ * @param {boolean} capitalize Si es `true` se capitaliza el texo.
  *
- * @return {String} Texto convertido.
+ * @return {string} Texto convertido.
  */
-function sep2cc(text, separator = '-')
+function sep2cc(text, separator = '-', capitalize = false)
 {
-    return String(text)
-        .replace(
-            new RegExp(`${regexcape(separator)}[${sep2cc.chars}]`, 'gu'),
-            match => match.substr(1).toUpperCase()
+    text = String(text);
+    if (text)
+    {
+        text = text.replace(
+            new RegExp(`[${regexcape(separator)}]+([${sep2cc.chars}])`, 'gu'),
+            (match, char) => char.toUpperCase()
         );
+        if (capitalize)
+        {
+            text = text[0].toUpperCase() + text.substr(1);
+        }
+    }
+
+    return text;
 }
 
 /**
  * Caracteres a tomar en cuenta como principio de palabra.
+ * No se diferencia entre mayúsculas y minúsculas.
  *
- * @type {RegExp}
+ * @type {string}
  */
 sep2cc.chars = 'a-zñáéíóú';
 //------------------------------------------------------------------------------
--- test.js
+++ test.js
@@ -5,23 +5,47 @@ let numAssertions = 0;
 function checkFn(fn, cases)
 {
     cases.forEach(
-        ([actual, expected]) =>
+        ([actual, expected, separator]) =>
         {
-            for (const _separator of ['_', '-', '.'])
+            if (separator)
             {
-                const _actual   = _separator
-                    ? actual.replace(/-/g, _separator)
-                    : actual;
-                const _expected = _separator
-                    ? expected.replace(/-/g, _separator)
-                    : expected;
-                const _result   = fn(_actual, _separator);
+                const _result = fn(actual, separator);
                 assert.equal(
                     _result,
-                    _expected,
-                    `${fn.name} ${_actual} [${_separator}]: ${_result} !== ${_expected}`
+                    expected,
+                    `${fn.name} ${actual} [${separator}]: ${_result} !== ${expected}`
                 );
                 ++numAssertions;
+            }
+            else
+            {
+                for (const _separator of ['_', '-', '.'])
+                {
+                    const _actual = _separator
+                        ? actual.replace(/-/g, _separator)
+                        : actual;
+                    let _expected = _separator
+                        ? expected.replace(/-/g, _separator)
+                        : expected;
+                    let _result   = fn(_actual, _separator);
+                    assert.equal(
+                        _result,
+                        _expected,
+                        `${fn.name} ${_actual} [${_separator}]: ${_result} !== ${_expected}`
+                    );
+                    ++numAssertions;
+                    if (expected)
+                    {
+                        _result   = fn(_actual, _separator, true);
+                        _expected = _expected[0].toUpperCase() + _expected.substr(1);
+                        assert.equal(
+                            _result,
+                            _expected,
+                            `${fn.name} ${_actual} [${_separator}]: ${_result} !== ${_expected}`
+                        );
+                        ++numAssertions;
+                    }
+                }
             }
         }
     );
@@ -36,7 +60,8 @@ checkFn(
         ['-once-upon-a-time', 'OnceUponATime'],
         ['-o-n-c-e -u-p-o-n -a -t-i-m-e', 'ONCE UPON A TIME'],
         ['inner-h-t-m-l', 'innerHTML'],
-        ['-áááá-éééé-íí-óóó', 'ÁáááÉéééÍíÓóó']
+        ['-áááá-éééé-íí-óóó', 'ÁáááÉéééÍíÓóó'],
+        ['-once-@_upon.:;a{}time', 'OnceUponATime', '-@_.:;{}']
     ]
 );
 console.log('Total aserciones: %d', numAssertions);
